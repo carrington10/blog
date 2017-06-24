ecb36d00535d027a8fc457e879655c6c465d3223
carrington <carrington10@ksu.edu>
2017-06-24 13:09:57 -0700

    added moderate comments features

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Post(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         return self.title
@@ -29,6 +32,7 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment = True
         self.save()
-
+    
+    
     def __str__(self):
         return self.text
